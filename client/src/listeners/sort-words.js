4afe42b84be9f84713be7f66fb97c6674cf311de
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-19 15:58:20 +0100

    Update sort-words.js

--- client/src/listeners/sort-words.js
+++ client/src/listeners/sort-words.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line folders/match-regex
 import { sortWords } from '../handlers/sort-words.js';
 
 document.getElementById('sort-type').addEventListener('change', sortWords);
