8a59eb30eb51e8822c6a4c3eed613bd8dd9527fd
denrique-alvarez <denrique.alvarez@gmail.com>
2022-03-02 11:59:49 +0100

    Update sort-strings.js

--- client/src/logic/sort-strings.js
+++ client/src/logic/sort-strings.js
@@ -19,4 +19,31 @@
  *
  * // ... write this!
  */
-export const sortStrings = (toSort = '', sortType = 'oldest') => {};
+export const sortStrings = (toSort = '', sortType = 'oldest') => {
+  const tempArray = [...toSort];
+
+  const sortLength = (par1, par2) => {
+    return par1.length - par2.length;
+  };
+
+  const sortCase = (par1, par2) => {
+    return par1.localeCompare(par2);
+  };
+
+  if (sortType === 'newest') {
+    return tempArray.reverse();
+  }
+  if (sortType === 'a') {
+    return tempArray.sort(sortCase);
+  }
+  if (sortType === 'z') {
+    return tempArray.sort(sortCase).reverse();
+  }
+  if (sortType === 'shortest') {
+    return tempArray.sort(sortLength);
+  }
+  if (sortType === 'longest') {
+    return tempArray.sort(sortLength).reverse();
+  }
+  return tempArray;
+};
